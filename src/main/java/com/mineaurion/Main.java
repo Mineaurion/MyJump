5954c5e06bf17ecaf3201ac7dc7318e9d298d174
Gaetan <dadoo18000@gmail.com>
2019-06-05 17:46:15 +0200

    [INIT] Version V2

---
+++ src/main/java/com/mineaurion/Main.java
@@ -0,0 +1,190 @@
+package com.mineaurion;
+
+import com.mineaurion.command.CommandCheckpoint;
+import com.mineaurion.command.CommandStopJump;
+import com.mineaurion.command.CommandUpdate;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.SQLException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.scheduler.BukkitScheduler;
+
+
+public class Main extends JavaPlugin {
+
+    public Jump jumpClass;
+    public Main instance=null;
+    public MySQLEngine mysqlEngine;
+
+
+    FileConfiguration config;
+
+
+    //Boot du plugin il executera cela pour bukkit :
+    @SuppressWarnings("deprecation")
+    @Override
+    public void onEnable() {
+        instance = this;
+        sendmessage("{{YELLOW}}MineJump Loading", "console");
+        initConfig();
+        initCommand();
+        initDatabase();
+        initEvents();
+
+        jumpClass = new Jump(this);
+
+        BukkitScheduler scheduler = getServer().getScheduler();
+        scheduler.scheduleAsyncRepeatingTask(this, new Runnable() {
+            @Override
+            public void run() {
+                jumpClass.updateScorebooard();
+            }
+        }, 0L, 20L);
+
+
+    }
+
+    public void initEvents() {
+        Bukkit.getServer().getPluginManager().registerEvents(new EventManager(this), this);
+    }
+
+    public void initDatabase() {
+        if(config.getString("Database.Address").equalsIgnoreCase("none")) {
+            sendmessage("{{DARK_RED}}Configure le plugin","console");
+        }else {
+            sendmessage("{{GOLD}}Loading DataBase", "console");
+            try {
+                mysqlEngine = new MySQLEngine(instance);
+            } catch (ClassNotFoundException | SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    public void initCommand() {
+
+        getCommand("checkpoint").setExecutor(new CommandCheckpoint(instance));
+        getCommand("stopjump").setExecutor(new CommandStopJump(instance));
+        getCommand("updatescore").setExecutor(new CommandUpdate(instance));
+    }
+
+    public void initConfig() {
+        getConfig().options().copyDefaults(true);
+        saveDefaultConfig();
+        config = instance.getConfig();
+        saveConfig();
+
+    }
+
+
+    public String prefix = "{{GREEN}}[{{YELLOW}}MineaurionJump{{GREEN}}]{{RESET}} ";
+
+    @SuppressWarnings("deprecation")
+    public void sendmessage(String message, String sender) {
+        if (sender.equalsIgnoreCase("console") || sender.equalsIgnoreCase("Server")) {
+            Bukkit.getConsoleSender().sendMessage(addColor(message));
+        } else {
+            Bukkit.getPlayer(sender).sendMessage(addColor(message));
+        }
+    }
+
+    private  String addColor(String message) {
+        message = prefix + message;
+        StringBuilder textmain = new StringBuilder();
+        Matcher m = Pattern.compile("(\\{\\{([^\\{\\}]+)\\}\\}|[^\\{\\}]+)").matcher(message);
+        ChatColor color = null;
+        ChatColor style = null;
+        while (m.find()) {
+
+            String entry = m.group();
+            if (entry.contains("{{")) {
+                color = null;
+                style = null;
+                switch (entry) {
+                    case "{{BLACK}}":
+                        color = ChatColor.BLACK;
+                        break;
+                    case "{{DARK_BLUE}}":
+                        color = ChatColor.DARK_BLUE;
+                        break;
+                    case "{{DARK_GREEN}}":
+                        color = ChatColor.DARK_GREEN;
+                        break;
+                    case "{{DARK_CYAN}}":
+                        color = ChatColor.DARK_AQUA;
+                        break;
+                    case "{{DARK_RED}}":
+                        color = ChatColor.DARK_RED;
+                        break;
+                    case "{{PURPLE}}":
+                        color = ChatColor.DARK_PURPLE;
+                        break;
+                    case "{{GOLD}}":
+                        color = ChatColor.GOLD;
+                        break;
+                    case "{{GRAY}}":
+                        color = ChatColor.GRAY;
+                        break;
+                    case "{{DARK_GRAY}}":
+                        color = ChatColor.DARK_GRAY;
+                        break;
+                    case "{{BLUE}}":
+                        color = ChatColor.AQUA;
+                        break;
+                    case "{{GREEN}}":
+                        color = ChatColor.GREEN;
+                        break;
+                    case "{{RED}}":
+                        color = ChatColor.RED;
+                        break;
+                    case "{{LIGHT_PURPLE}}":
+                        color = ChatColor.LIGHT_PURPLE;
+                        break;
+                    case "{{YELLOW}}":
+                        color = ChatColor.YELLOW;
+                        break;
+                    case "{{WHITE}}":
+                        color = ChatColor.WHITE;
+                        break;
+                    case "{{OBFUSCATED}}":
+                        style = ChatColor.MAGIC;
+                        break;
+                    case "{{BOLD}}":
+                        style = ChatColor.BOLD;
+                        break;
+                    case "{{STRIKETHROUGH}}":
+                        style = ChatColor.STRIKETHROUGH;
+                        break;
+                    case "{{UNDERLINE}}":
+                        style = ChatColor.UNDERLINE;
+                        break;
+                    case "{{ITALIC}}":
+                        style = ChatColor.ITALIC;
+                        break;
+                    case "{{RESET}}":
+                        style = ChatColor.RESET;
+                        break;
+                }
+            } else {
+                StringBuffer buff = new StringBuffer(entry);
+
+                if (color != null) {
+                    buff.insert(0, color);
+
+                }
+                if (style != null) {
+                    buff.insert(0, style);
+                }
+                textmain.append(buff);
+            }
+        }
+        return textmain.toString();
+    }
+}
+
---
+++ src/main/java/com/mineaurion/MySQLEngine.java
@@ -0,0 +1,135 @@
+package com.mineaurion;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import com.mineaurion.Main;
+
+public class MySQLEngine {
+    private Main plugin;
+    public String address;
+    public String port;
+    public String databaseName;
+    public String user;
+    public String password;
+    public String prefix;
+    private Connection connection;
+
+    public MySQLEngine(Main main) throws ClassNotFoundException, SQLException {
+        plugin = main;
+        address = plugin.config.getString("Database.Address");
+        port = plugin.config.getString("Database.Port");
+        databaseName = plugin.config.getString("Database.Db");
+        user = plugin.config.getString("Database.Username");
+        password = plugin.config.getString("Database.Password");
+        prefix = plugin.config.getString("Database.Prefix");
+
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection(
+                "jdbc:mysql://" + address + ":" + port + "/" + databaseName + "?autoReconnect=true", user, password);
+        connection
+                .prepareStatement("CREATE TABLE IF NOT EXISTS `" + prefix + "leader` ( "
+                        + "`uuid` VARCHAR(50) NOT NULL , " + "`name` VARCHAR(100) NOT NULL , "
+                        + "`time` INT NOT NULL , " + "PRIMARY KEY (`uuid`)" + ") ENGINE = InnoDB CHARSET=utf8;")
+                .execute();
+
+        connection
+                .prepareStatement("CREATE TABLE IF NOT EXISTS `" + prefix + "banned` ( "
+                        + "`uuid` VARCHAR(50) NOT NULL , " + "`name` VARCHAR(100) NOT NULL , "
+                        + "`time` INT NOT NULL , " + "PRIMARY KEY (`uuid`)" + ") ENGINE = InnoDB CHARSET=utf8;")
+                .execute();
+
+    }
+
+    public void setScorePlayer(String uuid, String name, int time) {
+        try {
+            PreparedStatement statement = null;
+            statement = connection.prepareStatement("SELECT time FROM " + prefix + "leader" + " WHERE uuid=?");
+            statement.setString(1, uuid);
+            ResultSet set = statement.executeQuery();
+            if (set.next()) {
+                if (time < set.getInt("time")) {
+                    updateScorePlayer(uuid, name, time);
+                }
+            } else {
+                updateScorePlayer(uuid, name, time);
+            }
+        } catch (SQLException e) {
+            plugin.sendmessage("{{DARK_RED}}" + e.getStackTrace().toString(), "console");
+            e.printStackTrace();
+        }
+    }
+
+    public void updateScorePlayer(String uuid, String name, int time) {
+        PreparedStatement statement = null;
+        try {
+            statement = connection
+                    .prepareStatement("REPLACE INTO " + prefix + "leader" + "(uuid,name,time) VALUES(?,?,?)");
+            statement.setString(1, uuid);
+            statement.setString(2, name);
+            statement.setInt(3, time);
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e) {
+            plugin.sendmessage("{{DARK_RED}}" + e.getStackTrace().toString(), "console");
+            e.printStackTrace();
+        }
+    }
+
+    public void AddBannedPlayer(String uuid, String name, int time) {
+        try {
+            PreparedStatement statement = null;
+            statement = connection
+                    .prepareStatement("REPLACE INTO " + prefix + "banned" + "(uuid,name,time) VALUES(?,?,?)");
+            statement.setString(1, uuid);
+            statement.setString(2, name);
+            statement.setInt(3, time);
+            statement.executeUpdate();
+            statement.close();
+        } catch (SQLException e) {
+            plugin.sendmessage("{{DARK_RED}}" + e.getStackTrace().toString(), "console");
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isBannedPlayer(String uuid) {
+        try {
+            PreparedStatement statement = null;
+            statement = connection.prepareStatement("SELECT * FROM " + prefix + "banned WHERE uuid=?");
+            statement.setString(1, uuid);
+            ResultSet set = statement.executeQuery();
+            if (set.next()) {
+                set.close();
+                statement.close();
+                return true;
+            } else {
+                return false;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            plugin.sendmessage("{{DARK_RED}}" + e.getStackTrace().toString(), "console");
+        }
+        return false;
+    }
+
+    public ResultSet getThirdPlayer() {
+        try {
+            PreparedStatement statement = null;
+            statement = connection.prepareStatement("SELECT * FROM " + prefix + "leader ORDER BY `time` LIMIT 3");
+            ResultSet set = statement.executeQuery();
+            return set;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            plugin.sendmessage("{{DARK_RED}}" + e.getStackTrace().toString(), "console");
+        }
+        return null;
+    }
+
+}
